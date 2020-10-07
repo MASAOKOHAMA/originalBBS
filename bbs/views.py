8dfcf29454c93f0234f4395e86a85914b362fa70
masaokohama <k7311111k610@icloud.com>
2020-10-07 11:40:40 +0900

    viewエラー

---
+++ bbs/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Article
+from .forms import SearchForm
+from .forms import ArticleForm
+
+def index(request):
+    searchForm = SearchForm(request.GET)
+    if searchForm.is_valid():
+        keyword = searchForm.cleaned_data['keyword']
+        articles = Article.objects.filter(content__contains=keyword)
+    else:
+        searchForm = SearchForm()
+        articles = Article.objects.all()
+
+    context = {
+        'message': 'Hello Django',
+        'articles': articles,
+        'searchForm': searchForm,
+    }
+    return render(request, 'bbs/index.html', context)
+
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    return HttpResponse(article)
+
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    context = {
+        'message': 'Show Article ' + str(id),
+        'article': article,
+    }
+    return render(request, 'bbs/detail.html', context)
+
+
+
+def create(request):
+    if request.method == 'POST':
+        articleForm = ArticleForm(request.POST)
+        if articleForm.is_valid():
+            article = articleForm.save()
+
+    context = {
+        'message': 'Create article ' + str(article.id),
+        'article': article,
+    }
+    return render(request, 'bbs/detail.html', context)
+
+def new(request):
+    articleForm = ArticleForm()
+
+    context = {
+        'message': 'New Article',
+        'articleForm': articleForm,
+    }
+    return render(request, 'bbs/new.html', context)
+
+def delete(request, id):
+    article = get_object_or_404(Article, pk=id)
+    article.delete()
+
+    articles = Article.objects.all()
+    context = {
+        'message': 'Delete article ' + str(id),
+        'articles': articles,
+    }
+    return render(request, 'bbs/index.html', context)
