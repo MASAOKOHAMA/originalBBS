409e00baa089cb6e09f5d4a01eea09296f713b50
masaokohama <k7311111k610@icloud.com>
2020-11-06 12:20:34 +0900

    BoardCreate修正

--- bbs/urls.py
+++ bbs/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-# from django.conf import settings
-# from django.conf.urls.static import static
 
 app_name = 'bbs'
 
@@ -10,12 +8,10 @@ urlpatterns = [
     path('<int:id>/detail', views.detail, name='detail'),
     path('new', views.new, name='new'),
     # path('create', views.create, name='create'),
-    path('BoardCreate', views.BoardCreate, name='BoardCreate'),
+    path('create', views.BoardCreate.as_view(), name='create'),
     path('<int:id>/delete', views.delete, name='delete'),
     path('<int:id>/edit', views.edit, name='edit'),
     path('<int:id>/update', views.update, name='update'),
     path('<int:id>', views.good, name='good'),
     path('<int:id>/favo', views.favo, name='favo'),
-    # path('<int:id>/comment', views.comment, name='comment'),
-    # path('<int:id>/comment_up', views.comment, name='comment_up'),
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- bbs/views.py
+++ bbs/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
 from .models import Article
 from .forms import SearchForm
 from .forms import ArticleForm
@@ -24,54 +23,17 @@ def index(request):
     return render(request, 'bbs/index.html', context)
 
 def good(request, id):
-# def good(id):
     article = get_object_or_404(Article, pk=id)
     article.good_count += 1
     article.save()
-    # print("good")
-    # print(article)
-    # print(article.good_count)
-    # good_count = get_object_or_404(Article, pk=id).good_count
-    # searchForm = SearchForm(request.GET)
-    # if searchForm.is_valid():
-    #     keyword = searchForm.cleaned_data['keyword']
-    #     articles = Article.objects.filter(content__contains=keyword)
-    # else:
-    #     searchForm = SearchForm()
-    #     articles = Article.objects.all()
-    # context = {
-    #     'message': 'Good Article ' + str(id),
-    #     'articles': articles,
-    #     'searchForm': searchForm,
-    #     'count': good_count
-    # }
-    # return render(request, 'bbs/index.html', context)
     return redirect("bbs:index")
 
 def favo(request, id):
-# def favo(id):
     article = get_object_or_404(Article, pk=id)
     article.favorite += 1
     article.favorite %= 2
     article.save()
-    # print("favo")
-    # print(article)
-    # print(article.favorite)
     favorite = get_object_or_404(Article, pk=id).favorite
-    # searchForm = SearchForm(request.GET)
-    # if searchForm.is_valid():
-    #     keyword = searchForm.cleaned_data['keyword']
-    #     articles = Article.objects.filter(content__contains=keyword)
-    # else:
-    #     searchForm = SearchForm()
-    #     articles = Article.objects.all()
-    # context = {
-    #     'message': 'Favorite Article ' + str(id),
-    #     'articles': articles,
-    #     'searchForm': searchForm,
-    #     'favorite': favorite
-    # }
-    # return render(request, 'bbs/index.html', context)
     return redirect("bbs:index")
 
 def delete(request, id):
@@ -109,23 +71,11 @@ def new(request):
     }
     return render(request, 'bbs/new.html', context)
 
-# def create(request):
-#     if request.method == 'POST':
-#         articleForm = ArticleForm(request.POST)
-#         if articleForm.is_valid():
-#             article = articleForm.save()
-#     context = {
-#         'message': 'Create article ' + str(article.id),
-#         'article': article,
-#         'articleForm': articleForm,
-#     }
-#     return render(request, 'bbs/detail.html', context)
-
 class BoardCreate(CreateView):
-    template_name = 'create.html'
+    template_name = 'bbs/new.html'
     model = Article
-    fields = ('title', 'content', 'images')
-    success_url = reverse_lazy('list')
+    fields = ('images', 'content')
+    success_url = reverse_lazy("bbs:index")
 
     def form_valid(self, form):
         form.instance.author = self.request.user
