cd2e5e671090c6bdc89cd4ff90b4fe8d2283505f
masaokohama <k7311111k610@icloud.com>
2020-12-10 22:35:05 +0900

    編集できない２

--- bbs/urls.py
+++ bbs/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('create', views.ArticleCreate.as_view(), name='create'),
     path('<int:id>/delete', views.delete, name='delete'),
     path('<int:id>/edit', views.edit, name='edit'),
-    path('<int:id>/update', views.update, name='update'),
+    # path('<int:id>/update', views.update, name='update'),
+    path('<int:id>/update', views.ArticleUpdate.as_view(), name='update'),
     path('<int:id>', views.good, name='good'),
     path('<int:id>/favo', views.favo, name='favo'),
 ]
--- bbs/views.py
+++ bbs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView
 from .models import Article
 from .forms import SearchForm
 from .forms import ArticleForm
@@ -80,6 +80,20 @@ class ArticleCreate(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super(CreateView, self).form_valid(form)
+
+
+class ArticleUpdate(UpdateView, id):
+    template_name = 'bbs/new.html'
+    model = Article
+    fields = ('images', 'content', 'user_name')
+    success_url = reverse_lazy("bbs:index")
+
+    def get_success_url(self):
+        return reverse('bbs:index', kwargs={'id': self.article.id})
+
+    # def form_valid(self, form):
+    #     form.instance.author = self.request.user
+    #     return super(UpdateView, self).form_valid(form)
 
 def edit(request, id):
     article = get_object_or_404(Article, pk=id)
